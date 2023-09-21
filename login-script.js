5a618f0462f909ffb099f1e7ef463a41e2fba11c
Anirban Das <anirban.d.2003@gmail.com>
2023-09-22 01:10:56 +0530

    commit 22sept

--- login-script.js
+++ login-script.js
@@ -1,25 +1,22 @@
-// login-script.js
 document.addEventListener("DOMContentLoaded", function () {
-    const loginContainer = document.getElementById("login-container");
-    const registrationContainer = document.getElementById("registration-container");
-    const registerBtnLogin = document.getElementById("register-btn-login");
-    const registerBtnRegistration = document.getElementById("register-btn-registration");
-    const backToLoginBtn = document.getElementById("back-to-login-btn");
+   const loginContainer = document.getElementById("login-container");
+   const registrationContainer = document.getElementById("registration-container");
+   const registerBtnLogin = document.getElementById("register-btn-login");
+   const backToLoginBtn = document.getElementById("back-to-login-btn");
 
-    registerBtnLogin.addEventListener("click", () => {
-        loginContainer.style.display = "none";
-        registrationContainer.style.display = "block";
-        registrationContainer.style.left = "10%";
-        registrationContainer.style.width = "30%";
-        registrationContainer.style.height = "100%";
-        registrationContainer.style.top = "0%";
-        
-        
-    });
+   registerBtnLogin.addEventListener("click", () => {
+      loginContainer.classList.add("slide-out");
+      registrationContainer.style.display = "block";
+      setTimeout(() => {
+         registrationContainer.classList.add("slide-in");
+      }, 50);
+   });
 
-
-    backToLoginBtn.addEventListener("click", () => {
-        registrationContainer.style.display = "none";
-        loginContainer.style.display = "block"; // Show the login container again
-    });
+   backToLoginBtn.addEventListener("click", () => {
+      registrationContainer.classList.remove("slide-in");
+      setTimeout(() => {
+         loginContainer.classList.remove("slide-out");
+         registrationContainer.style.display = "none";
+      }, 500);
+   });
 });
